d7a7a3384ecfb900dd9fa876c6bb48e9c81ccf33
mohammednaushad <naushadsufiyan123@gmail.com>
2025-07-09 23:34:34 +0530

    New files

---
+++ student.sql
@@ -0,0 +1,13 @@
+create database colllege;
+use colllege;
+
+create table student(
+	id int primary key,
+    name varchar(50),
+    age int not null
+	);
+    
+insert into student values (1,"nau",21);
+insert into student values (2,"dad",20);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
+select * from student;
