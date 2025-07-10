f76f359d22b4a9e3c0b690ce3c1e23a6ff14816e
mohammednaushad <naushadsufiyan123@gmail.com>
2025-07-10 20:21:03 +0530

    java prac

---
+++ referncee.java
@@ -0,0 +1,18 @@
+
+import java.awt.Point;
+
+public class referncee {
+    public static void main(String[] args) {
+        //In Primitive the value doesnt chnages 
+        int x= 3;
+        int y=x;
+        x=4;
+        System.out.println(y); 
+
+        //In reference or non premitive
+        Point point1 = new Point(1, 1);
+        Point point2 =
+
+
+    }
+}
---
+++ types.java
@@ -0,0 +1,16 @@
+
+
+public class types {
+    public static void main(String[] args) {
+        byte age = 30;
+        int viewscount = 123_354_699;
+        long viewsexpected = 123_354_699_3;
+        float price = 10.22f;
+        double pricebig = 10.33;
+        char letter = 'a';
+        boolean isEligible = false;
+        System.out.println(price);
+        
+
+    }
+}
